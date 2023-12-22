7d8731b8a921cd219c5a9a6fb8f016a7b18ae0f8
Stefan <decimelli@ibm.com>
2023-12-21 21:32:13 -0500

    make edits

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -3,9 +3,10 @@ import styles from "./Banner.module.css"
 export default function Banner() {
     return (
         <div className={styles.container}>
-            <div className={styles.pContainer}>
-                <p>Full-stack Development with React.js • Next.js and PERN Stack Applications</p>
-                <hr />
+            <div className={styles.bannerContent}>
+                <strong>Hello, welcome to my website!</strong>
+                <p>I am a full-stack developer who specializes in React.js apps. Right now you are on a website made with React.js, hosted on AWS using AWS Amplify. If you want a website designed and develped by me, then feel free to <a target="_blank" href="https://www.linkedin.com/in/stefandecimelli/"><u>contact me on LinkedIn.</u></a></p>
+                <p>or,</p>
                 <a target="_blank" href="https://github.com/stefandecimelli">
                     <button>Check me out on GitHub  </button>
                 </a>
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -6,22 +6,13 @@ const projects = [
         name: "Sine Wave Visualizer",
         href: "https://www.sinewave.stefandecimelli.ca/",
         description: "A small tool app that can display what a sine wave looks like at a specific magnitude and frequency."
-    },
-    { 
-        name: "Sine Wave Visualizer",
-        href: "https://www.sinewave.stefandecimelli.ca/",
-        description: "A small tool app that can display what a sine wave looks like at a specific magnitude and frequency."
-    },
-    { 
-        name: "Sine Wave Visualizer",
-        href: "https://www.sinewave.stefandecimelli.ca/",
-        description: "A small tool app that can display what a sine wave looks like at a specific magnitude and frequency."
     }
 ]
 
 export default function Projects() {
     return (
         <div className={styles.container}>
+             <p>Click any of the links below to see what kind of things I am working on.</p>
             <h6>Projects</h6>
             <div className={styles.projectsList}>
             {
